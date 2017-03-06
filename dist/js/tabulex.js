e02e98488294e9c2cb48ba43e3c84d01ef6ca774
Uffe Petersen <uffe.petersen@ist.com>
2017-03-06 16:04:18 +0100

    Added login + guide page

--- dist/js/tabulex.js
+++ dist/js/tabulex.js
@@ -27,4 +27,47 @@ $(document).ready(function () {
     $('[data-toggle="offcanvas"]').click(function () {
         $('#main-container').toggleClass('toggled');
     });
+
+    // Adding functionality to collapsed elements
+    (function() {
+        $(".panel-heading a").click(function(){
+            var collapse_elem = $(this).closest('.panel').find('.panel-collapse');
+            collapse_elem.collapse('toggle');
+            $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
+        });
+    })();
+
+    // Adding highlight syntax to all pre code elements
+    (function() {
+        $('pre code').each(function (i, block) {
+            hljs.highlightBlock(block);
+        });
+    })();
+
+    // Adding copy/paste button for each pre element
+    (function(){
+        var pre = document.getElementsByTagName('pre');
+        for (var i = 0; i < pre.length; i++) {
+            var button = document.createElement('button');
+            button.className = 'btn btn-default btn-xs btn-clipboard';
+            button.textContent = 'Copy';
+
+            pre[i].appendChild(button);
+        }
+
+        var copyCode = new Clipboard('.btn-clipboard', {
+            target: function(trigger) {
+                return trigger.previousElementSibling;
+            }
+        });
+
+        copyCode.on('success', function(event) {
+            event.clearSelection();
+            event.trigger.textContent = 'Copied';
+            window.setTimeout(function() {
+                event.trigger.textContent = 'Copy';
+            }, 2000);
+        });
+    })();
+
 });
