265de426aa045c6abf4fd6084471deff5b212fc6
Uffe Petersen <uffe.petersen@ist.com>
2017-06-07 15:42:48 +0200

    Prototyping children grid

--- resources/views/layouts/menus/top_menu_parent.blade.php
+++ resources/views/layouts/menus/top_menu_parent.blade.php
@@ -114,7 +114,7 @@
                         <li><a href="{{ route('basic-grid') }}">Basic grid</a></li>
                         <li><a href="{{ route('login-demo') }}">Login demo</a></li>
                         <li><a href="{{ route('personal-data') }}">Stamkort</a></li>
-                        <li><a href="{{ route('child-grid') }}">Children grid</a></li>
+                        <li><a href="{{ route('find-boern') }}">Find børn</a></li>
                         <li><a href="{{ route('parents-desktop') }}">Opslagstavle - for&aelig;ldre</a></li>
                         <li><a href="{{ route('messages') }}">Kommunikation</a></li>
                         <li><a href="{{ route('readmessages') }}">Læs/svar beskeder</a></li>
